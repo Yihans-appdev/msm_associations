c45d6d091aec865fffecee0f4d8aa70ee01c08cf
Yihan <yihan.tan@gmail.com>
2017-11-11 04:44:43 +0000

    Lots done

---
+++ app/models/actor.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  dob        :string
+#  bio        :text
+#  image_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Actor < ApplicationRecord
+  validates :name,
+    presence: true,
+    uniqueness: { scope: :dob, message: "with this DOB already exists" }
+end
